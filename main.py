a2c5a608a71456fde76c5dbcc008a40a1eb2b914
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2021-09-30 00:39:59 +0200

    add track, artist_to_genre, artist_to_album, album

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlalchemy as sq
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship, mapper
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -67,8 +67,8 @@ artist_to_album = sq.Table(
 
 artist_to_genre = sq.Table(
     'artist_to_genre', Base.metadata,
-    sq.Column('artist_id', sq.Integer, sq.ForeignKey(Artist.id)),
-    sq.Column('genre_id', sq.Integer, sq.ForeignKey(Genre.id))
+    sq.Column('artist_id', sq.ForeignKey(Artist.id)),
+    sq.Column('genre_id', sq.ForeignKey(Genre.id))
 )
 
 track_to_collection = sq.Table(
@@ -115,14 +115,110 @@ if __name__ == '__main__':
     # for i in genre:
     #     genre_obj = Genre(name=i)
     #     session.add(genre_obj)
+
+    # for i in album.items():
+    #     album_obj = Album(name=i[0], release_year=i[1])
+    #     session.add(album_obj)
+
     artist_genre = {
         'Kaiser_Chiefs': 'Indie', 'Franz_Ferdinand': 'Indie', 'Led_Zeppelin': 'Hard_Rock', 'Nirvana': 'Grunge',
         'FKJ': 'Electronics', 'Jose_James': 'Jazz', 'Blink-182': 'Punk-rock', 'Massive_Attack': 'Trip-Hop'
     }
 
-    for i in artist_genre.items():
-        a = session.query(Artist).filter(Artist.name == i[0]).first()
-        g = session.query(Genre).filter(Genre.name == i[1]).first()
-        print(a.name)
+    # for i in artist_genre.items():
+    #     a = session.query(Artist).filter(Artist.name == i[0]).first()
+    #     g = session.query(Genre).filter(Genre.name == i[1]).first()
+    #     a.genre.append(g)
+    #     session.add(a)
+    artist_album = {
+        'Kaiser_Chiefs': ['Duck'], 'Franz_Ferdinand': ['Always_Acsending'], 'Led_Zeppelin': ['Led_Zeppelin_IV'],
+        'Nirvana': ['Nevermind'], 'FKJ': ['Just_Piano'], 'Jose_James': ['Lean_On_Me'], 'Blink-182': ['California'],
+        'Massive_Attack': ['Helogoland']
+    }
+    #
+    # for i in artist_album.items():
+    #     artist = session.query(Artist).filter(Artist.name == i[0]).first()
+    #     for j in i[1]:
+    #         album = session.query(Album).filter(Album.name == j).first()
+    #         album.artist.append(artist)
+    #         session.add(album)
 
+    track = [
+        {'track_name': 'People_Know_How_To_Love_One_Another',
+         'album': 'Duck',
+         'len': 216},
+        {'track_name': 'Golden_Oldies',
+         'album': 'Duck',
+         'len': 244},
+        {'track_name': 'Wait',
+         'album': 'Duck',
+         'len': 230},
+        {'track_name': 'Always_Acsending',
+         'album': 'Always_Acsending',
+         'len': 321},
+        {'track_name': 'Lazy_Boy',
+         'album': 'Always_Acsending',
+         'len': 179},
+        {'track_name': 'Paper_Cages',
+         'album': 'Always_Acsending',
+         'len': 220},
+        {'track_name': 'Black_Dog',
+         'album': 'Led_Zeppelin_IV',
+         'len': 296},
+        {'track_name': 'Rock_And_Roll',
+         'album': 'Led_Zeppelin_IV',
+         'len': 221},
+        {'track_name': 'The_Battle_of_Evermore',
+         'album': 'Led_Zeppelin_IV',
+         'len': 352},
+        {'track_name': 'Smells_Like_Teen_Spirit',
+         'album': 'Nevermind',
+         'len': 301},
+        {'track_name': 'In_Bloom',
+         'album': 'Nevermind',
+         'len': 255},
+        {'track_name': 'Come_As_You_Are',
+         'album': 'Nevermind',
+         'len': 219},
+        {'track_name': 'Sundays',
+         'album': 'Just_Piano',
+         'len': 213},
+        {'track_name': 'Anthem',
+         'album': 'Just_Piano',
+         'len': 157},
+        {'track_name': 'Last_Hour',
+         'album': 'Just_Piano',
+         'len': 163},
+        {'track_name': 'Lean_On_Me',
+         'album': 'Lean_On_Me',
+         'len': 297},
+        {'track_name': 'Cynical',
+         'album': 'California',
+         'len': 116},
+        {'track_name': "She_is_out_Of_Her_Mind",
+         'album': 'California',
+         'len': 163},
+        {'track_name': 'Bored_to_Death',
+         'album': 'California',
+         'len': 236},
+        {'track_name': 'Prey_For_Rain',
+         'album': 'Helogoland',
+         'len': 404},
+        {'track_name': 'Babel',
+         'album': 'Helogoland',
+         'len': 320},
+        {'track_name': 'Splitting_The_Atom',
+         'album': 'Helogoland',
+         'len': 317}
+    ]
+
+    for i in track:
+        track_name = i['track_name']
+        album_name = i['album']
+        track_len = i['len']
+        album_id = session.query(Album).filter(Album.name == album_name).first()
+        track_for_add = Track(name=track_name, album_id=album_id.id, length=track_len)
+        session.add(track_for_add)
+
+    session.commit()
 
